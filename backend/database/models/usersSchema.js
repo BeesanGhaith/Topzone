2be271925a2631cad77ceba54765de0d8694b18c
BeesanGhaith <beesan.ghaith@gmail.com>
2022-06-14 19:44:47 +0300

    create users schema

---
+++ backend/database/models/usersSchema.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const usersSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  password: { type: String, required: true },
+  country: { type: String },
+});
+
+usersSchema.pre("save", async function () {
+  this.email = this.email.toLowerCase();
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+module.exports = mongoose.model("User", usersSchema);
