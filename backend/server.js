395a0fb1f822ed16dec9f7c6cbcc6fcb526c87a4
BeesanGhaith <beesan.ghaith@gmail.com>
2022-06-14 19:49:30 +0300

    import routers in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,25 @@
 // require express
 const express = require("express");
 
-// require the file to execute it
-const db = require("./database/db");
+const cors = require("cors");
 
 // instantiate express
 const app = express();
 
+// require the file to execute it
+const db = require("./database/db");
+
+app.use(cors());
+
 app.use(express.json());
+
+// Import Routers
+const usersRouter = require("./routes/user");
+const loginRouter = require("./routes/login");
+
+// Routes Middleware
+app.use("/users", usersRouter);
+app.use("/login", loginRouter);
 
 const PORT = 5000;
 app.listen(PORT, () => {
