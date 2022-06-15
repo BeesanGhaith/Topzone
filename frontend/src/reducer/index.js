464718fa164b22d24f547d912b91336affca174a
BeesanGhaith <beesan.ghaith@gmail.com>
2022-06-15 16:06:10 +0300

    create store

---
+++ frontend/src/reducer/index.js
@@ -0,0 +1,11 @@
+// Store..
+import { combineReducers, createStore } from "redux";
+import loginReducer from "./login/index";
+const reducers = combineReducers({ loginReducer });
+
+// Create Store
+const store = createStore(
+  reducers,
+ );
+
+export default store;
